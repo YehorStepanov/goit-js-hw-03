95544f1600af6482d0e2dc5cd16baa592cdaa588
YehorStepanov <stepegorr@gmail.com>
2025-06-11 15:44:20 +0300

    make all

--- js/task-2.js
+++ js/task-2.js
@@ -1,14 +1,29 @@
-function formatMessage(message, maxLength) {
-    if (message.length > maxLength) {
-        return message.slice(0, maxLength) + '...';
+function makeArray(firstArray, secondArray, maxLength) {
+    let array = [];
+
+    if (firstArray.length <= maxLength) {
+        for (const element of firstArray) {
+            array.push(element);
+        }
+        maxLength -= firstArray.length;
+    } else {
+        for (let i = 0; i < maxLength; i++){
+            array.push(firstArray[i])
+        }
     }
-    return message;
+    if (maxLength > 0) {
+        for (let i = 0; i < maxLength; i++){
+            array.push(secondArray[i])
+        }
+    }
+
+    return array;
 }
-console.log(formatMessage("Curabitur ligula sapien", 16));
-console.log(formatMessage("Curabitur ligula sapien", 23));
-console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
 
 
