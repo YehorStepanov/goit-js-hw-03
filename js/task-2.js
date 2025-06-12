211d5e73cf78ee98a71b5780174662d645338ce4
YehorStepanov <stepegorr@gmail.com>
2025-06-12 14:32:12 +0300

    fix second task

--- js/task-2.js
+++ js/task-2.js
@@ -1,23 +1,9 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    let array = [];
-
-    if (firstArray.length <= maxLength) {
-        for (const element of firstArray) {
-            array.push(element);
-        }
-        maxLength -= firstArray.length;
-    } else {
-        for (let i = 0; i < maxLength; i++){
-            array.push(firstArray[i])
-        }
-    }
-    if (maxLength > 0) {
-        for (let i = 0; i < maxLength; i++){
-            array.push(secondArray[i])
-        }
-    }
-
-    return array;
+    const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength); 
+  }
+  return newArray; 
 }
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
